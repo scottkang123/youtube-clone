cdaab4e0321bf001c1d085f2e65b1884fec9c19b
scottkang123 <48114611+scottkang123@users.noreply.github.com>
2023-12-28 01:09:58 +0900

    lec 2

--- backend/src/main/java/com/programming/techie/youtubeclone/service/FileService.java
+++ backend/src/main/java/com/programming/techie/youtubeclone/service/FileService.java
@@ -1,5 +1,9 @@
 package com.programming.techie.youtubeclone.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface FileService {
+
+    String uploadFile(MultipartFile file);
 
 }
--- backend/src/main/java/com/programming/techie/youtubeclone/service/S3Service.java
+++ backend/src/main/java/com/programming/techie/youtubeclone/service/S3Service.java
@@ -1,5 +1,54 @@
 package com.programming.techie.youtubeclone.service;
 
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.util.StringUtils;
+
+//import com.amazonaws.util.StringUtils;
+
+@Service
+@RequiredArgsConstructor
 public class S3Service implements FileService {
+
+    private AmazonS3Client awsS3Client;
+
+    @Override
+    public String uploadFile(MultipartFile file) {
+        // upload file to aws s3
+
+        // prepare a key
+        var filenameExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+
+        var key = UUID.randomUUID().toString() + filenameExtension;
+
+        var metadata = new ObjectMetadata();
+
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+
+        try {
+            awsS3Client.putObject("youtube-clone1234", key, file.getInputStream(), metadata);
+        } catch (IOException ioException) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    "An exception occurred while uploading the files");
+        }
+
+        awsS3Client.setObjectAcl("youtube-clone1234", key, CannedAccessControlList.PublicRead);
+
+        return awsS3Client.getResourceUrl("youtube-clone1234", key);
+
+    }
 
 }
